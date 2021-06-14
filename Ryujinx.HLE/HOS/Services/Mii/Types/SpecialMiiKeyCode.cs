38ccd11abb0f7f8ae46423ae35ab36b859c13c7f
Mary <me@thog.eu>
2021-06-14 22:03:10 +0200

    sdb: Fix inverted logic for special mii in AddOrReplace

--- Ryujinx.HLE/HOS/Services/Mii/Types/SpecialMiiKeyCode.cs
+++ Ryujinx.HLE/HOS/Services/Mii/Types/SpecialMiiKeyCode.cs
@@ -11,7 +11,7 @@ namespace Ryujinx.HLE.HOS.Services.Mii.Types
 
         public bool IsEnabledSpecialMii()
         {
-            return false; // RawValue == SpecialMiiMagic; // FIX ME
+            return RawValue == SpecialMiiMagic;
         }
     }
 }
